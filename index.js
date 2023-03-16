9daf6baa59c49a06b6ab70804803bad95cb49cc4
Kelvin Muturi <muturikelvin893@gmail.com>
2023-03-16 23:08:16 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+// Sample implementation of the three functions
+function divide() {
+    return 2000 / 100;
+  }
+  
+  const square = (x) => {
+    return x * x;
+  }
+  
+  const add = (a, b) => {
+    return a + b;
+  }
+  
+  // Example test suite using Mocha and Chai
+  const expect = require('chai').expect;
+  
+  describe("index.js", () => {
+    it("has a function expression called divide", () => {
+      expect(divide).to.exist;
+    });
+  
+    it("divide divides 2000 by 100", () => {
+      expect(divide()).to.equal(20);
+    });
+  
+    it("has an arrow function called square", () => {
+      expect(square).to.exist;
+    });
+  
+    it("square arrow function takes one parameter and multiplies it times itself", () => {
+      let x = 2;
+      expect(square(x)).to.equal(4);
+    });
+  
+    it("has an arrow function called add", () => {
+      expect(add).to.exist;
+    });
+  
+    it("add arrow function takes two parameters and adds them together", () => {
+      let a = 3;
+      let b = 4;
+      expect(add(a, b)).to.equal(a + b);
+    });
+  });
